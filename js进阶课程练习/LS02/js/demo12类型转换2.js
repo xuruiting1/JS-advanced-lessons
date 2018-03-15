0069e15b77f4e983ee5d4d3ce75a0639007b5cff
徐瑞婷 <1263183452@qq.com>
2018-03-15 14:42:35 +0800

    Number、String、Boolean相关方法

---
+++ js进阶课程练习/LS02/js/demo12类型转换2.js
@@ -0,0 +1,34 @@
+//隐式类型转换
+
+
+// 流程语句
+var h = {x:1};
+//var h = "";
+if(h){
+    console.log("h:",h);
+}
+// 输出h: {x: 1}
+
+//比较运算符 
+var a = 1;
+var b = 2;
+console.log(typeof (a>b),a>b);
+console.log(typeof (a==b),a==b);
+console.log(typeof (a<b),a<b);
+
+//boolean false
+// boolean false
+// boolean true
+
+//算数运算符 
+var c = "img" + 1 +".jpg";
+var d = "12" - 1;
+console.log(c,d);
+
+//img1.jpg 11
+
+//逻辑运算符
+var e = !12;
+var f = !!34;
+var g = !!{};
+console.log(e,f,g);//输出false true true
