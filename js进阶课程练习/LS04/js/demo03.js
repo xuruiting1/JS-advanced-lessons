335fd816d81d87fdbfb7d4c898d38564f39b536c
徐瑞婷 <1263183452@qq.com>
2018-03-24 14:33:02 +0800

    JS语法、表达式及语句

--- js进阶课程练习/LS04/js/demo03.js
+++ js进阶课程练习/LS04/js/demo03.js
@@ -4,3 +4,20 @@ if(true)
 }
 console.log(a);
 //输出20
+
+for(var i = 0;i<5;i++){
+    console.log("in for ",i);
+}
+console.log("out of for ",i);//报错么，输出啥？
+//不会报错，输出
+//in for  0
+// in for  1
+//in for  2
+// in for  3
+//in for  4
+//out of for  5
+
+if(false){
+    var b = 30;
+}
+console.log(b);//undefinned
